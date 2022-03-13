1278b5c6cecad60245bb79699030d0455f2246e7
Isacc Barker <plutoisplanet4324@gmail.com>
2022-03-12 20:00:29 -0700

    transfer

--- include/svm_class.h
+++ include/svm_class.h
@@ -1,9 +1,16 @@
-#ifndef SVM_CLASS
-#define SVM_CLASS
+#ifndef SVM_CLASS_H
+#define SVM_CLASS_H
 
-struct svm_class {
-    int tmp;
-};
+#include <stdint.h>
 
-#endif /* SVM_CLASS */
+/** A representation of a class containing JVM bytecode.
+ * In the specification, this is defined under the 4th chapter (4.0). */
+typedef struct {
+    uint32_t magic;
+    uint16_t minor_version;
+    uint16_t major_version;
+    uint16_t constant_pool_count;
+} svm_constant_pool_entry ;
+
+#endif /* SVM_CLASS_H */
 
---
+++ include/svm_constant_pool.h
@@ -0,0 +1,4 @@
+#ifndef SVM_CONSTANT_POOL_H
+#define SVM_CONSTANT_POOL_R
+
+#endif /** */
